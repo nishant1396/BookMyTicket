41d6f1510525f3566f03ceace1f330cc002570c9
KodeKrunch <hkatakiya1995@gmail.com>
2019-01-23 20:53:48 +0530

    Temp check

--- src/main/java/com/booking/controller/AdminController.java
+++ src/main/java/com/booking/controller/AdminController.java
@@ -31,7 +31,7 @@ public class AdminController {
 	public String adminRegistration() {
 		
 		return "adminRegistration";
-		
+	
 	}
 	
 	/* ------------------------- Account detail ----------------------*/
